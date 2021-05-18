8471a927c23ee658d6b82cee98b086adc608cb5e
Khushbaz Singh <KH060@atcltd.atc.org.nz>
2021-05-19 10:16:17 +1200

    Database script

--- SchoolFinalProject/DatabaseScript/data.sql
+++ SchoolFinalProject/DatabaseScript/data.sql
@@ -1,1 +1,15 @@
-﻿
+﻿SET IDENTITY_INSERT [dbo].[School] ON
+INSERT INTO [dbo].[School] ([Id], [School_Name], [School_Branch]) VALUES (1, N'Dsp', N'Auckland')
+INSERT INTO [dbo].[School] ([Id], [School_Name], [School_Branch]) VALUES (2, N'Sps', N'Auckland')
+INSERT INTO [dbo].[School] ([Id], [School_Name], [School_Branch]) VALUES (3, N'Asi', N'Auckland')
+SET IDENTITY_INSERT [dbo].[School] OFF
+SET IDENTITY_INSERT [dbo].[Tutor] ON
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (1, N'Ainm', N'Auckland', N'0226677898')
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (2, N'Sit', N'Auckland', N'022656789')
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (3, N'Anny', N'Auckland', N'022688990')
+SET IDENTITY_INSERT [dbo].[Tutor] OFF
+SET IDENTITY_INSERT [dbo].[Tutor] ON
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (1, N'Ainm', N'Auckland', N'0226677898')
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (2, N'Sit', N'Auckland', N'022656789')
+INSERT INTO [dbo].[Tutor] ([Id], [Tutor_Name], [Tutor_Address], [Tutor_Contact]) VALUES (3, N'Anny', N'Auckland', N'022688990')
+SET IDENTITY_INSERT [dbo].[Tutor] OFF
